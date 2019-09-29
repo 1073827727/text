be2463de421699144fee0d73a0f5665bf49beb0b
guo <1073827727@qq.com>
2019-09-29 01:41:38 +0000

    初始化样式

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -6,5 +6,22 @@ use Illuminate\Http\Request;
 
 class StaticPagesController extends Controller
 {
-    //
+    public function home(){
+        return view('StaticPages.home');
+    }
+
+    public function help(){
+        return view('StaticPages.help');
+    }
+
+    public function about(){
+        return view('StaticPages.about');
+    }
+
+
+
+
+
+
+
 }
